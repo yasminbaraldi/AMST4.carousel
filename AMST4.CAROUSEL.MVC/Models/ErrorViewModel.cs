06e2f790bfc09cdce26c18929b03ee1a0798bc1f
yasminbaraldi <163891328+yasminbaraldi@users.noreply.github.com>
2024-04-15 18:58:12 -0300

    FirstComit

---
+++ AMST4.CAROUSEL.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AMST4.CAROUSEL.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
